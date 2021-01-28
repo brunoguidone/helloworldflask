aa9acea36d91878ecad8980229c875ea263c596c
root <brunoguidone@hotmail.com>
2021-01-27 23:42:51 -0300

    Blue-Green deployment

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! v1.2"
 
 if __name__ == "__main__":
     app.run(debug=False, port=5000, host="0.0.0.0")
