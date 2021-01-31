c61a9751991b352918c3ea6e1737ed8f0acf428d
root <brunoguidone@hotmail.com>
2021-01-30 21:17:30 -0300

    canary

--- app/main.py
+++ app/main.py
@@ -5,5 +5,20 @@ app = Flask(__name__)
 def hello():
     return "Hello World! v1.2"
 
+counter = 1
+
+@app.route("/count")
+def count():
+    global counter
+    counter += 1
+    mensagem = f"Versao 1.2. Contagem: {counter}"
+    return mensagem
+
+@app.route("/reset")
+def reset():
+    global counter
+    counter = 1
+    return str(counter)
+
 if __name__ == "__main__":
     app.run(debug=False, port=5000, host="0.0.0.0")
